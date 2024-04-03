48dcc45e5dd4a0664f6e38da435c737a89714ee5
mrsshumailkhan <165428870+mrsshumailkhan@users.noreply.github.com>
2024-04-03 07:59:52 +0500

    Question9

---
+++ 9_Favorite_Number/Favorite_Number.js
@@ -0,0 +1,7 @@
+// Store your favorite number in a variable
+let favoriteNumber = 12;
+// Create a message revealing your favorite number
+let message = `My favorite number is ${favoriteNumber}.`;
+// Print the message
+console.log(message);
+export {};
