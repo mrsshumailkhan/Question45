1ee62d49063512f3bbee7f5b5bb478d151853ec1
mrsshumailkhan <165428870+mrsshumailkhan@users.noreply.github.com>
2024-04-03 06:44:37 +0500

    Question5

---
+++ 4_Famous_Quote/main.js
@@ -0,0 +1,6 @@
+// Define the quote and its author
+const quote = "A person who never made a mistake never tried anything new.";
+const author = "Albert Einstein";
+// Print the quote and its author
+console.log(`${author} once said, "${quote}"`);
+export {};
---
+++ 4_Famous_Quote/main.ts
@@ -0,0 +1,8 @@
+// Define the quote and its author
+
+const quote: string = "A person who never made a mistake never tried anything new.";
+const author: string = "Albert Einstein";
+
+// Print the quote and its author
+
+console.log(`${author} once said, "${quote}"`);
