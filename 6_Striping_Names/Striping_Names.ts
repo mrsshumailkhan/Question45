2ef486b9b07585a5bef854d46946dcf30bd6d7ad
mrsshumailkhan <165428870+mrsshumailkhan@users.noreply.github.com>
2024-04-03 07:34:09 +0500

    Question6

---
+++ 6_Striping_Names/Striping_Names.ts
@@ -0,0 +1,11 @@
+// Store the person's name with whitespace characters
+let personName: string = "\t \n   Shumail Khan \n\t ";
+
+// Print the name with whitespace
+console.log( personName);
+
+// Remove leading and trailing whitespace using trim()
+let without_Whitespace : string = personName.trim();
+
+// Print the stripped name
+console.log(without_Whitespace);
