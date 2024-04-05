1abb8c14ef4ba5e2af8a209196ddf0c628999d79
mrsshumailkhan <165428870+mrsshumailkhan@users.noreply.github.com>
2024-04-05 07:24:52 +0500

    14_Guest_List

---
+++ 14_Guest_List/Guest_List.js
@@ -0,0 +1,7 @@
+// Define an array to store the names of people you'd like to invite to dinner
+let guestList = ["Imran Khan", "Bill Gates", "Elon Musk"];
+// Iterate over the array and print an invitation message to each person
+for (let i = 0; i < guestList.length; i++) {
+    console.log(`Dear Mr. ${guestList[i]},\n\nYou are cordially invited to dinner at my place.\n\n Thankyou. `);
+}
+export {};
