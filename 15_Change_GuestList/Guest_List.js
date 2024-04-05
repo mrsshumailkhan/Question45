c9b935fb0b81c7790047555372d9a39b94b715cc
mrsshumailkhan <165428870+mrsshumailkhan@users.noreply.github.com>
2024-04-05 07:47:51 +0500

    15_Change_Guestlist

---
+++ 15_Change_GuestList/Guest_List.js
@@ -0,0 +1,14 @@
+// Define an array to store the names of people you'd like to invite to dinner
+let guestList = ["Imran Khan", "Bill Gates", "Elon Musk"];
+// Iterate over the array and print an invitation message to each person
+for (let i = 0; i < guestList.length; i++) {
+    console.log(`Dear Mr. ${guestList[i]},\n\nYou are cordially invited to dinner at my place.\n\n Thankyou. `);
+}
+let absent_guest = "Bill Gates";
+let new_guest = "Kamran Tessori";
+guestList[0] = new_guest;
+for (let i = 0; i < guestList.length; i++) {
+    console.log(`Dear Mr. ${guestList[i]},\n\nYou are cordially invited to dinner at my place.\n\n Thankyou. `);
+}
+console.log(`${absent_guest} is not comming for dinner `);
+export {};
