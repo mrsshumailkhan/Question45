463cece8119dc29271886d0648d9ebc4320f3120
mrsshumailkhan <165428870+mrsshumailkhan@users.noreply.github.com>
2024-04-05 07:01:43 +0500

    13_Own_Array

---
+++ 13_Own Array/Own_Array.js
@@ -0,0 +1,7 @@
+// Define an array to store examples of favorite modes of transportation
+let transportationModes = ["motorcycle", "car", "bicycle", "scooter"];
+// Iterate over the array and print statements about each item
+for (let i = 0; i < transportationModes.length; i++) {
+    console.log(`I would like to buy a ${transportationModes[i]}.`);
+}
+export {};
