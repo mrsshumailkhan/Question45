4078bf1b9c3ede2e7ccfe37b81f09abf9f1b16d7
mrsshumailkhan <165428870+mrsshumailkhan@users.noreply.github.com>
2024-04-05 06:55:23 +0500

    12_Greetings

---
+++ 12_Greetings/Greetings.js
@@ -0,0 +1,7 @@
+// Storing names in an array called names
+let names = ["Khizer", "Azeen", "Khalil", "Shumail"];
+// Printing a personalized message to each person
+for (let i = 0; i < names.length; i++) {
+    console.log(`Hello, ${names[i]}! Welcome to the Typescript Class!`);
+}
+export {};
---
+++ 12_Greetings/Greetings.ts
@@ -0,0 +1,8 @@
+// Storing names in an array called names
+let names: string[] = ["Khizer", "Azeen", "Khalil", "Shumail"];
+
+// Printing a personalized message to each person
+
+for (let i = 0; i < names.length; i++) {
+    console.log(`Hello, ${names[i]}! Welcome to the Typescript Class!`);
+}
