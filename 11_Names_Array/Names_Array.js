8c939baca9b2449505318560c3d8994f23ceed2a
mrsshumailkhan <165428870+mrsshumailkhan@users.noreply.github.com>
2024-04-05 06:13:47 +0500

    11_Names_Array

---
+++ 11_Names_Array/Names_Array.js
@@ -0,0 +1,7 @@
+// Storing names in an array called names
+let names = ["Khizer", "Azeen", "Khalil", "Shumail"];
+// Printing each person's name by accessing each element in the list one at a time
+for (let i = 0; i < names.length; i++) {
+    console.log(names[i]);
+}
+export {};
