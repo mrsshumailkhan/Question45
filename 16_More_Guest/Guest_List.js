436ceec683257aa73dfc04ca120f640cd5d3bebf
mrsshumailkhan <165428870+mrsshumailkhan@users.noreply.github.com>
2024-04-05 08:13:07 +0500

    16_MoreGuests

---
+++ 16_More_Guest/Guest_List.js
@@ -0,0 +1,20 @@
+let guestList = ["Imran Khan", "Bill Gates", "Elon Musk"];
+// Iterate over the array and print an invitation message to each person
+// for (let i = 0; i < guestList.length; i++) {
+//     console.log(`Dear Mr. ${guestList[i]},\n\nYou are cordially invited to dinner at my place.\n\n Thankyou. `);
+// }
+let absent_guest = "Imran Khan";
+let new_guest = "Kamran Tessori";
+guestList[0] = new_guest;
+for (let i = 0; i < guestList.length; i++) {
+    console.log(`Dear Mr. ${guestList[i]},\n\nYou are cordially invited to dinner at my place.\n\n Thankyou. `);
+}
+console.log(`${absent_guest} is not comming for dinner `);
+console.log("Good News! we find big table so we are inviting 3 more guest.");
+guestList.unshift("Sir Zia Khan");
+guestList.splice(2, 0, 'Daniyal Nagori');
+guestList.push("sir Ameen");
+for (let i = 0; i < guestList.length; i++) {
+    console.log(`Dear Mr. ${guestList[i]},\n\nYou are cordially invited to dinner at my place.\n\n Thankyou. `);
+}
+export {};
