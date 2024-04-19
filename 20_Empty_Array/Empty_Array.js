807b9d6446238eacc7fbb2e908c1038345fe422a
mrsshumailkhan <165428870+mrsshumailkhan@users.noreply.github.com>
2024-04-19 11:52:12 +0500

    20_Empty_Array

---
+++ 20_Empty_Array/Empty_Array.js
@@ -0,0 +1,9 @@
+"use strict";
+//Think of something you could store in array.
+let items = ['Pakistan', 'Karachi', 'Urdu'];
+//write a programe that create a list containing these item.
+let item2 = [];
+item2.push('Germany');
+item2.push('England');
+item2.push('India');
+console.log(item2);
---
+++ 20_Empty_Array/Empty_Array.ts
@@ -0,0 +1,12 @@
+//Think of something you could store in array.
+let items :string[] = ['Pakistan', 'Karachi', 'Urdu'];
+
+//write a programe that create a list containing these item.
+//store items in array with the help of .push .
+let item2 :string[] = [] ;
+
+item2.push('Germany');
+item2.push('England');
+item2.push('India');
+
+console.log(item2);
