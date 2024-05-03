927c3f0fe4b0949b41e836e3ba1de0158a3f22d8
mrsshumailkhan <165428870+mrsshumailkhan@users.noreply.github.com>
2024-05-03 08:40:46 +0500

    23_Condiyional_Tests

---
+++ 23_Conditiona_Tests/conditional_tests.ts
@@ -0,0 +1,59 @@
+
+// Write a series of conditional tests.
+
+//creating car veriable
+let car :string = 'subaru';
+
+// Test No 1
+console.log("Is car === 'subaru'? I predict True.");
+console.log(car ==='subaru' );
+
+// Test no 2 
+console.log("Is car == 'subaru'? I predict True.");
+console.log(car == 'subaru' );
+
+// Test no 3
+console.log("Is car != 'Honda'? I predict True.");
+console.log(car != 'Honda' );
+
+// Test no 4
+console.log("Is car !== 'ford'? I predict True.");
+console.log(car !== 'ford')
+
+
+// Test no 5
+console.log("Is car.Upper case == 'SUBARU'? I predict True.");
+console.log(car.toUpperCase() == 'SUBARU');
+
+// Test no 6
+console.log("Is car == 'SUBARU'? I predict false.");
+console.log(car == 'SUBARU');
+
+// Test no 7
+console.log("Is car === 'SUBARU'? I predict false.");
+console.log(car === 'SUBARU');
+
+// Test no 8
+console.log("Is car === 'Train'? I predict false.");
+console.log(car === 'Train');
+
+// Test no 9
+console.log("Is car === 'Bus'? I predict false.");
+console.log(car === 'Bus');
+
+// Test no 9
+console.log("Is car === 'Cycle'? I predict false.");
+console.log(car === 'Cycle');
+
+
+
+
+
+
+//  =   is ka matlab hum kisi value ko set kr rhy hain.
+
+//  ==  hum kisi value ko check kr rhy hain.
+
+/// === hum kisi value ko uski datatypes ke sath check kar rhy hain.
+
+// !=    Not equal 
