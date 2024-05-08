bd9796bef35c6a5c513e21614a66cc7db3d6aec9
mrsshumailkhan <165428870+mrsshumailkhan@users.noreply.github.com>
2024-05-08 07:53:36 +0500

    25_alien_colour

---
+++ 25_Alien_colour/25_alien_colour1.ts
@@ -0,0 +1,14 @@
+// create a veriable called alien colour.
+let alien_colour : string = 'green';
+
+if(alien_colour === 'green'){
+    console.log("Player just earned 5 points !");
+}
+
+//write 1 version of this programe that passes the if test  and another test fails.
+alien_colour ='Yellow';
+
+//the version that fails will have no output.
+if(alien_colour === 'green'){
+    console.log("Player just earned 5 points !");
+}
