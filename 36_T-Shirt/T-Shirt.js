3c2bc6ae2588a98ec969b20dd9ed7b4410a64e11
mrsshumailkhan <165428870+mrsshumailkhan@users.noreply.github.com>
2024-05-10 08:43:58 +0500

    36_T-Shirt

---
+++ 36_T-Shirt/T-Shirt.js
@@ -0,0 +1,10 @@
+"use strict";
+//T-Shirt: Write a function called make_shirt() that accepts a size and the text of a message that should be printed on the shirt.
+// The function should print a sentence summarizing the size of the shirt and the message printed on it. Call the function.
+//call the function
+function make_shirt(size, text) {
+    console.log(`Creating a ${size} shirt with the message: ${text}`);
+}
+make_shirt('large', 'Hello kitty');
+make_shirt('medum', 'Hello kitty');
+make_shirt('small', 'Hello kitty');
