2f663d498874acafe4589cbc0feba71782f9f952
mrsshumailkhan <165428870+mrsshumailkhan@users.noreply.github.com>
2024-05-12 21:42:11 +0500

    43_UnChanged_Magician

---
+++ 43_Unchanged_Magicians/unChanged.ts
@@ -0,0 +1,27 @@
+
+// Define the function to show magicians name.
+function show_magicians(magicians: string[]){
+      magicians.forEach(name => console.log(name));
+}
+
+//function to make magicians great through .map it will modifay array.
+function make_great(magicians: string[]){
+   return magicians.map(name => `The Great ${name}`);
+}
+    
+// Define an array of magician names.
+let magicians_name = ['Harry poter','Dai Vernon','Doug Henning'];
+
+//Making a copy of orignal array through .slice() function.
+let copy_magician_names = magicians_name.slice() 
+
+// Modify the copied array to include "The Great" with thier names.
+let copy_great_magicians = make_great(copy_magician_names);
+
+// Show both arrays orignal and copied.
+
+// Orignal
+show_magicians(magicians_name);
+
+// Copied
+show_magicians(copy_great_magicians);
