d6a6e8ef4847cd6945425baa8af1d64c998ea9a2
mrsshumailkhan <165428870+mrsshumailkhan@users.noreply.github.com>
2024-05-12 19:12:51 +0500

    42_greatMagician

---
+++ 42_Great_Magician/greatMagician.js
@@ -0,0 +1,15 @@
+"use strict";
+// Define the function to show magicians name.
+function show_magicians(magicians) {
+    magicians.forEach(name => console.log(name));
+}
+//function to make magicians great through .map it will modifay array.
+function make_great(magicians) {
+    return magicians.map(name => `The Great ${name}`);
+}
+// Define an array of magician names.
+let magicians_name = ['Harry poter', 'Dai Vernon', 'Doug Henning'];
+// Call make_great function to modify magicians name.
+let great_magicians = make_great(magicians_name);
+//Call show_magicians that show modified list of magicians.
+show_magicians(great_magicians);
